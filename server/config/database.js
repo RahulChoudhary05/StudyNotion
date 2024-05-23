206a58291964f9e60e9a60a61dfcde1a917460ef
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-05-23 18:27:23 +0530

    Create all models

---
+++ server/config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+require("dotenv").config(); //all data in envirement(.env) file, that load by dotenv package
+
+exports.connect = () => {
+  mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("DB Connection Successfully!"))
+    .catch((error) => {
+      console.log("Issue in DB Connection!");
+      console.log(error.message);
+      //either 0 or 1. 0 means end the process without any kind of failure and 1 means end the process with some failure {exit the Node.js process with a status code of 1 when that file is executed.}.
+      process.exit(1);
+    });
+};
