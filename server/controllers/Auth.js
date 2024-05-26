238f5365113901c0f844cf45dc0315b596735fc5
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-05-26 11:39:16 +0530

    OTP generated logic done

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -1,4 +1,75 @@
+const User = require("../models/User");
+const OTP = require("../models/OTP");
+const otpGenerator = require("otp-generator");
+
 //sendotp
+exports.sendOTP = async (req, res) => {
+  try {
+    //fetch email from request body
+    const { email } = req.body;
+
+    //check if user already exist
+    const checkUserPresent = await User.findOne({ email });
+
+    // if user already User.exists, then return response
+    if (checkUserPresent) {
+      return res.status(401).json({
+        success: false,
+        message: "User already registered",
+      });
+    }
+
+    //generate OTP
+    var otp = otpGenerator.generate(6, {
+      upperCaseAlphabets: false,
+      lowerCaseAlphabets: false,
+      specialChars: false,
+    });
+    console.log("OTP Generated: ", otp);
+
+    //check unique otp or not
+    let result = await OTP.findOne({ otp: otp });
+    while (result) {
+      otp = otpGenerator(6, {
+        upperCaseAlphabets: false,
+        lowerCaseAlphabets: false,
+        specialChars: false,
+      });
+      result = await OTP.findOne({ otp: otp });
+    }
+
+    //otp entry in db
+    const otpPayload = { email, otp };
+    const otpBody = await OTP.create(otpPayload);
+    console.log(otpBody);
+
+    //return the resonse successfully
+    res.status(200).json({
+      success: true,
+      message: "OTP send successfully",
+      otp,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
 //signup
+expects.signUp = async(req,res)=>{
+    //data fetch from request body
+    //validate it
+    //2 password mactch (password === conformpassword)
+    //check user already exist or not
+    // find most resent OTP stored in db for the user
+    //validate otp
+    //hash password
+    //entry create in db(store user datails)
+    //return res
+};
+
 //login
 //changePassword
