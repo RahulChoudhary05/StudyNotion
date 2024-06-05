63e09e2570f9bc43b803124478c9078fbcf6f05e
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-06-05 18:51:34 +0530

    create Course handler function

--- server/controllers/Course.js
+++ server/controllers/Course.js
@@ -7,17 +7,25 @@ const { uploadImageToCloudinary } = require("../utils/imageUploader");
 exports.createCourse = async (req, res) => {
   try {
     //fetch the data from db
-    const {courseName, courseDescription, whatYouWillLearn, price, tag} = req.body;
+    const { courseName, courseDescription, whatYouWillLearn, price, tag } =
+      req.body;
 
     //get thumbnail
-    const  thumbnail = req.files.thumbnailImage;
+    const thumbnail = req.files.thumbnailImage;
 
     //validation
-    if(!courseName || !courseDescription || !whatYouWillLearn || !price || !tag || !thumbnail){
-    return res.status(400).json({
-      success: false,
-      message: "All fields are required",
-    });
+    if (
+      !courseName ||
+      !courseDescription ||
+      !whatYouWillLearn ||
+      !price ||
+      !tag ||
+      !thumbnail
+    ) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
     }
 
     //check for instructor
@@ -25,28 +33,75 @@ exports.createCourse = async (req, res) => {
     const instructorDetails = await User.findById(userID);
     console.log("Instructor Details :", instructorDetails);
 
-    if(!instructorDetails){
-    return res.status(404).json({
-      success: false,
-      message: "Instructor details not found",
-    });
+    if (!instructorDetails) {
+      return res.status(404).json({
+        success: false,
+        message: "Instructor details not found",
+      });
     }
 
     //check given tag is valid or not
     const tagDetails = await Tag.findById(tag);
-    if(!tagDetails){
-        return res.status(404).json({
-            success: false,
-            message: "Tags details not found",
-          });
+    if (!tagDetails) {
+      return res.status(404).json({
+        success: false,
+        message: "Tags details not found",
+      });
     }
 
     //upload Image top cloudinary
+    const thumbnailImage = await uploadImageToCloudinary(
+      thumbnail,
+      process.env.FOLDER_NAME
+    );
+
+    //create an entry for new course
+    const newCourse = await Course.create({
+      courseName,
+      courseDescription,
+      instructor: instructorDetails._id,
+      whatYouWillLearn,
+      price,
+      tag: tagDetails._id,
+      thumbnail: thumbnailImage.secure_url,
+    });
+
+    //add the new course to the user schema of Instructor
+    await User.findByIdAndUpdate(
+      { _id: instructorDetails._id }, //find out the entry of this instructor
+      {
+        // user array add the course id
+        $push: {
+          courses: newCourse._id,
+        },
+      },
+      { new: true }
+    );
+
+    //update the tag schema
+    await Tag.findByIdAndUpdate(
+      { _id: tagDetails._id }, //find out the entry of this tag
+      {
+        // Add the newly created course to the courses array
+        $push: {
+          courses: newCourse._id,
+        },
+      },
+      { new: true }
+    );
+
+    //return response
+    return res.status(200).json({
+      success: true,
+      message: "Course created successfully",
+      data: newCourse,
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({
       success: false,
-      message: "Something went wrong in Course section",
+      message: "Failed to create course",
+      error: error.message,
     });
   }
 };
