d2841c6ad157c3fc547289e245d5f889ac95ef9f
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-06-14 16:50:31 +0530

    in category page fetch all data and different data

--- server/controllers/Category.js
+++ server/controllers/Category.js
@@ -1,3 +1,4 @@
+const Category = require("../models/Category");
 const Tag = require("../models/Category");
 
 //create tag handler function
@@ -41,6 +42,61 @@ exports.showAllCategory = async (req, res) => {
       success: true,
       message: "All Tags return Successfully",
       allTags,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+//categoryPageDetails
+exports.categoryPageDetails = async (req, res) => {
+  try {
+    //get categoryId
+    const { categoryId } = req.body;
+
+    // get courses for specified categoryId
+    const selectedCategory = await Category.findById(categoryId)
+      .populate("Courses")
+      .exec();
+
+    //validation
+    if (!selectedCategory) {
+      return res.status(400).json({
+        success: false,
+        message: "Data not found",
+      });
+    }
+    //get courses for different category
+    const differentCategories = await Category.find({
+      _id: { $ne: categoryId }, // ne means not equal
+    })
+      .populate("courses")
+      .exec();
+
+    // Get top-selling courses across all categories
+    const allCategories = await Category.find()
+      .populate({
+        path: "courses",
+        match: { status: "Published" },
+        populate: {
+          path: "instructor",
+        },
+      })
+      .exec();
+    const allCourses = allCategories.flatMap((category) => category.courses);
+    const mostSellingCourses = allCourses
+      .sort((a, b) => b.sold - a.sold)
+      .slice(0, 10);
+    // console.log("mostSellingCourses COURSE", mostSellingCourses)
+    //return responses
+    return res.status(200).json({
+      success: true,
+      message: "All Tags return Successfully",
+      data: { selectedCategory, differentCategories, mostSellingCourses },
     });
   } catch (error) {
     console.log(error);
