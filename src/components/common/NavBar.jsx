f963fe259aa5b7d95d9342e56f0161344750c5d3
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-07-11 18:47:58 +0530

    in NavBar fix the logic, signup, card, profile logic design

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,15 +1,38 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux"
+import { AiOutlineMenu, AiOutlineShoppingCart } from "react-icons/ai";
 import { Link, matchPath } from 'react-router-dom'
 import logo from "../../assets/Logo/Logo-Full-Light.png"
 import { BsChevronDown } from "react-icons/bs"
 import { NavbarLinks } from "../../data/navbar-links"
 import { useLocation } from 'react-router-dom';
+import { ACCOUNT_TYPE } from "../../utils/constants"
+import ProfileDropdown from "../core/Auth/ProfileDropdown"
+import { apiConnector } from "../../services/apiconnector"
+import { categories } from "../../services/apis"
 
 export const NavBar = () => {
+  const { token } = useSelector((state) => state.auth)
+  const { user } = useSelector((state) => state.profile)
+  const { totalItems } = useSelector((state) => state.cart)
+
     const location = useLocation()
 
     const [subLinks, setSubLinks] = useState([])
     const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+      ;(async () => {
+        setLoading(true)
+        try {
+          const res = await apiConnector("GET", categories.CATEGORIES_API)
+          setSubLinks(res.data.data)
+        } catch (error) {
+          console.log("Could not fetch Categories.", error)
+        }
+        setLoading(false)
+      })()
+    }, [])
 
     const matchRoute = (route) => {
         return matchPath({ path: route }, location.pathname)
@@ -87,6 +110,38 @@ export const NavBar = () => {
             ))}
           </ul>
         </nav>
+
+        {/* Login/SignUp/Dashboard */}
+        <div className="hidden items-center gap-x-4 md:flex">
+          {user && user?.accountType !== ACCOUNT_TYPE.INSTRUCTOR && (
+            <Link to="/dashboard/cart" className="relative">
+              <AiOutlineShoppingCart className="text-2xl text-richblack-100" />
+              {totalItems > 0 && (
+                <span className="absolute -bottom-2 -right-2 grid h-5 w-5 place-items-center overflow-hidden rounded-full bg-richblack-600 text-center text-xs font-bold text-yellow-100">
+                  {totalItems}
+                </span>
+              )}
+            </Link>
+          )}
+          {token === null && (
+            <Link to="/login">
+              <button className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100">
+                Log in
+              </button>
+            </Link>
+          )}
+          {token === null && (
+            <Link to="/signup">
+              <button className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100">
+                Sign up
+              </button>
+            </Link>
+          )}
+          {token !== null && <ProfileDropdown />}
+        </div>
+        <button className="mr-4 md:hidden">
+          <AiOutlineMenu fontSize={24} fill="#AFB2BF" />
+        </button>
         </div>
     </div>
   )
---
+++ src/services/apis.js
@@ -0,0 +1,6 @@
+const BASE_URL = "https://localhost:4000"
+
+// CATAGORIES API
+export const categories = {
+    CATEGORIES_API: BASE_URL + "/course/showAllCategories",
+  }
