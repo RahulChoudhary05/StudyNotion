9d041ebeb73f90b56e485baaf4ee5a4bb0d5acc8
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-08-13 23:11:24 +0530

    Fix the signUp verification

--- src/services/operations/authAPI.js
+++ src/services/operations/authAPI.js
@@ -75,6 +75,7 @@ export function signUp(
       navigate("/login")
     } catch (error) {
       console.log("SIGNUP API ERROR............", error)
+      toast.error(error.response?.data?.message || "Signup Failed");
       toast.error("Signup Failed")
       navigate("/signup")
     }
