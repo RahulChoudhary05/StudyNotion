f09f1800273863e06688433feae2d438d76fdaa2
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-08-25 18:59:37 +0530

    created a forntend of dashboard of myprofile with side slider

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ import UpdatePassword from "./pages/UpdatePassword";
 import VerifyEmail from "./pages/VerifyEmail";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import MyProfile from "./components/core/Dashboard/MyProfile";
+import Dashboard from "./pages/Dashboard";
+import PrivateRoute from "./components/core/Auth/PrivateRoute";
+import Error from "./pages/Error";
 
 function App() {
   return (
@@ -64,9 +68,19 @@ function App() {
             <About />
           }
         />
-        <Route path="/contact" element={<Contact/>} />
+        <Route path="/contact" element={<Contact />} />
+        <Route
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        >
+          <Route path="dashboard/my-profile" element={<MyProfile />} />
+        </Route>
+        <Route path="*" element={<Error/>}/>
       </Routes>
-    </div>
+    </div >
   );
 }
 
