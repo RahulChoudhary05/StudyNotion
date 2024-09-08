247a04df2c96f89cf83f88746e0fe6222bda871b
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-09-08 18:37:25 +0530

    created cart page with secure route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
 import Home from "./pages/Home";
 import OpenRoute from "./components/core/Auth/OpenRoute";
-
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import { NavBar } from "./components/common/NavBar";
@@ -17,8 +18,13 @@ import PrivateRoute from "./components/core/Auth/PrivateRoute";
 import Error from "./pages/Error";
 import Settings from "./components/core/Dashboard/Settings";
 import EnrolledCourses from "./components/core/Dashboard/EnrolledCourses";
+import Cart from "./components/core/Dashboard/Cart";
+import { ACCOUNT_TYPE } from "./utils/constants";
 
 function App() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => state.profile)
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
       <NavBar />
@@ -80,9 +86,17 @@ function App() {
         >
           <Route path="dashboard/my-profile" element={<MyProfile />} />
           <Route path="dashboard/Settings" element={<Settings />} />
-          <Route path="dashboard/enrolled-courses" element={<EnrolledCourses/>}/>
+
+          {
+            user?.accountType === ACCOUNT_TYPE.STUDENT && (
+              <>
+                <Route path="dashboard/cart" element={<Cart />} />
+                <Route path="dashboard/enrolled-courses" element={<EnrolledCourses />} />
+              </>
+            )
+          }
         </Route>
-        <Route path="*" element={<Error/>}/>
+        <Route path="*" element={<Error />} />
       </Routes>
     </div >
   );
