9b5cbd72a3115f7f9ce78f59f276a47a9bd27812
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-09-11 20:52:24 +0530

    created courseinformationform 50% add ChipInput, RequirementsField, editcourse, dataaddin course

--- src/components/core/Dashboard/AddCourse/CourseInformation/ChipInput.jsx
+++ src/components/core/Dashboard/AddCourse/CourseInformation/ChipInput.jsx
@@ -1,12 +1,94 @@
-import React from 'react'
-import { CiShoppingTag } from "react-icons/ci";
+import { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
 
-export default function ChipInput() {
+import { MdClose } from "react-icons/md"
+
+export default function ChipInput({
+  // Props to be passed to the component
+  label,
+  name,
+  placeholder,
+  register,
+  errors,
+  setValue,
+  getValues,
+}) {
+  const { editCourse, course } = useSelector((state) => state.course)
+
+  const [chips, setChips] = useState([])
+
+  useEffect(() => {
+    if (editCourse) {
+      setChips(course?.tag)
+    }
+    register(name, { required: true, validate: (value) => value.length > 0 })
+  }, [])
+
+  useEffect(() => {
+    setValue(name, chips)
+  }, [chips])
+
+  const handleKeyDown = (event) => {
+    // Check if user presses "Enter" or ","
+    if (event.key === "Enter" || event.key === ",") {
+      // Prevent the default behavior of the event
+      event.preventDefault()
+      // Get the input value and remove any leading/trailing spaces
+      const chipValue = event.target.value.trim()
+      // Check if the input value exists and is not already in the chips array
+      if (chipValue && !chips.includes(chipValue)) {
+        // Add the chip to the array and clear the input
+        const newChips = [...chips, chipValue]
+        setChips(newChips)
+        event.target.value = ""
+      }
+    }
+  }
+
+  // deletion chip
+  const handleDeleteChip = (chipIndex) => {
+    // Filter the chips array to remove the chip with the given index
+    const newChips = [...chips].splice((chipIndex,1));
+    setChips(newChips)
+  }
+
   return (
     <div className="flex flex-col space-y-2">
-        <label className="text-sm text-richblack-5 flex" htmlFor="courseTags">
-        Tags <sup className="text-pink-200">*</sup>
-        </label>
+      <label className="text-sm text-richblack-5" htmlFor={name}>
+        {label} <sup className="text-pink-200">*</sup>
+      </label>
+
+      <div className="flex w-full flex-wrap gap-y-2">
+        {chips.map((chip, index) => (
+          <div
+            key={index}
+            className="m-1 flex items-center rounded-full bg-yellow-400 px-2 py-1 text-sm text-richblack-5"
+          >
+            {chip}
+            <button
+              type="button"
+              className="ml-2 focus:outline-none"
+              onClick={() => handleDeleteChip(index)}
+            >
+              <MdClose className="text-sm" />
+            </button>
+          </div>
+        ))}
+        {/* Render the input for adding new chips */}
+        <input
+          id={name}
+          name={name}
+          type="text"
+          placeholder={placeholder}
+          onKeyDown={handleKeyDown}
+          className="form-style w-full"
+        />
+      </div>
+      {errors[name] && (
+        <span className="ml-2 text-xs tracking-wide text-pink-200">
+          {label} is required
+        </span>
+      )}
     </div>
-  ) 
+  )
 }
--- src/components/core/Dashboard/AddCourse/CourseInformation/CourseInformationForm.jsx
+++ src/components/core/Dashboard/AddCourse/CourseInformation/CourseInformationForm.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useForm } from "react-hook-form"
 import { useDispatch, useSelector } from 'react-redux';
+import { toast } from "react-hot-toast"
 
 import { TbReceiptRupee  } from "react-icons/tb";
 import { MdNavigateNext } from "react-icons/md"
@@ -10,22 +11,24 @@ import {
   editCourseDetails,
   fetchCourseCategories,
 } from "../../../../../services/operations/courseDetailsAPI"
+import { setCourse, setStep } from "../../../../../slices/courseSlice"
+import { COURSE_STATUS } from "../../../../../utils/constants"
 import IconBtn from "../../../../common/IconBtn"
 import Upload from "../Upload"
 import ChipInput from "./ChipInput"
 import RequirementsField from "./RequirementField"
 
 export default function CourseInformationForm() {
-  const{
+  const {
     register,
     handleSubmit,
     setValue,
     getValues,
     formState: { errors },
-  } = useForm();
+  } = useForm()
 
   const dispatch = useDispatch()
-  // const { token } = useSelector((state) => state.auth)
+  const { token } = useSelector((state) => state.auth)
   const { course, editCourse } = useSelector((state) => state.course)
   const [loading, setLoading] = useState(false)
   const [courseCategories, setCourseCategories] = useState([])
@@ -52,12 +55,101 @@ export default function CourseInformationForm() {
     }
     getCategories()
   }, [])
+
+  const isFormUpdated = () => {
+    const currentValues = getValues()
+    if (
+      currentValues.courseTitle !== course.courseName ||
+      currentValues.courseShortDesc !== course.courseDescription ||
+      currentValues.coursePrice !== course.price ||
+      currentValues.courseTags.toString() !== course.tag.toString() ||
+      currentValues.courseBenefits !== course.whatYouWillLearn ||
+      currentValues.courseCategory._id !== course.category._id ||
+      currentValues.courseRequirements.toString() !==
+        course.instructions.toString() ||
+      currentValues.courseImage !== course.thumbnail
+    ) {
+      return true
+    }
+    return false
+  }
+
+  const onSubmit = async (data) => {
+    if (editCourse) {
+
+      if (isFormUpdated()) {
+        const currentValues = getValues()
+        const formData = new FormData()
+        // console.log(data)
+        formData.append("courseId", course._id)
+        if (currentValues.courseTitle !== course.courseName) {
+          formData.append("courseName", data.courseTitle)
+        }
+        if (currentValues.courseShortDesc !== course.courseDescription) {
+          formData.append("courseDescription", data.courseShortDesc)
+        }
+        if (currentValues.coursePrice !== course.price) {
+          formData.append("price", data.coursePrice)
+        }
+        if (currentValues.courseTags.toString() !== course.tag.toString()) {
+          formData.append("tag", JSON.stringify(data.courseTags))
+        }
+        if (currentValues.courseBenefits !== course.whatYouWillLearn) {
+          formData.append("whatYouWillLearn", data.courseBenefits)
+        }
+        if (currentValues.courseCategory._id !== course.category._id) {
+          formData.append("category", data.courseCategory)
+        }
+        if (
+          currentValues.courseRequirements.toString() !==
+          course.instructions.toString()
+        ) {
+          formData.append(
+            "instructions",
+            JSON.stringify(data.courseRequirements)
+          )
+        }
+        if (currentValues.courseImage !== course.thumbnail) {
+          formData.append("thumbnailImage", data.courseImage)
+        }
+        // console.log("Edit Form data: ", formData)
+        setLoading(true)
+        const result = await editCourseDetails(formData, token)
+        setLoading(false)
+        if (result) {
+          dispatch(setStep(2))
+          dispatch(setCourse(result))
+        }
+      } else {
+        toast.error("No changes made to the form")
+      }
+      return
+    }
+
+    const formData = new FormData()
+    formData.append("courseName", data.courseTitle)
+    formData.append("courseDescription", data.courseShortDesc)
+    formData.append("price", data.coursePrice)
+    formData.append("tag", JSON.stringify(data.courseTags))
+    formData.append("whatYouWillLearn", data.courseBenefits)
+    formData.append("category", data.courseCategory)
+    formData.append("status", COURSE_STATUS.DRAFT)
+    formData.append("instructions", JSON.stringify(data.courseRequirements))
+    formData.append("thumbnailImage", data.courseImage)
+    setLoading(true)
+    const result = await addCourseDetails(formData, token)
+    if (result) {
+      dispatch(setStep(2))
+      dispatch(setCourse(result))
+    }
+    setLoading(false)
+  }
   return (
     <form
-      // onSubmit={handleSubmit(onSubmit)}
+      onSubmit={handleSubmit(onSubmit)}
       className="space-y-8 rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-6"
     >
-      {/* Course Title */}
+
       <div className="flex flex-col space-y-2">
         <label className="text-sm text-richblack-5" htmlFor="courseTitle">
           Course Title <sup className="text-pink-200">*</sup>
