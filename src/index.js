9403b425ffd4bfa61cabba839b01568bf6e9fb04
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-11-28 18:53:00 +0530

    fix error in index

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const store = configureStore({
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider>
       <BrowserRouter>
         <App />
         <Toaster />
