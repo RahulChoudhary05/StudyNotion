27205bf4c7a5da6d92997b5ec4281a53f2fe83f2
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-11-28 18:53:33 +0530

    add store in index file

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const store = configureStore({
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
         <Toaster />
