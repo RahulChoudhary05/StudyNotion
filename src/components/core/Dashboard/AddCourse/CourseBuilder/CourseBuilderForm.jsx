88fb2a1723ec5208b83414bdad613d5a06f1c18e
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-11-28 18:51:15 +0530

    add react-redux pack and fix course section

--- src/components/core/Dashboard/AddCourse/CourseBuilder/CourseBuilderForm.jsx
+++ src/components/core/Dashboard/AddCourse/CourseBuilder/CourseBuilderForm.jsx
@@ -3,6 +3,7 @@ import { useForm } from "react-hook-form"
 import { toast } from "react-hot-toast"
 import { IoAddCircleOutline } from "react-icons/io5"
 import { MdNavigateNext } from "react-icons/md"
+import { useDispatch, useSelector } from "react-redux"
 
 import {
   createSection,
@@ -14,6 +15,7 @@ import {
   setStep,
 } from "../../../../../slices/courseSlice"
 import IconBtn from "../../../../common/IconBtn"
+import NestedView from "./NestedView"
 
 export default function CourseBuilderForm() {
   const {
