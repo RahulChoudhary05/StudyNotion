73345b47756b93e0a4718d5d6936eb5d608b443a
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2025-02-16 20:37:46 +0530

    update the contect details

--- src/components/ContactPage/ContactDetails.jsx
+++ src/components/ContactPage/ContactDetails.jsx
@@ -17,12 +17,6 @@ const contactDetails = [
     details:
       "Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016",
   },
-  {
-    icon: "IoCall",
-    heading: "Call us",
-    description: "Mon - Fri From 8am to 5pm",
-    details: "+123 456 7869",
-  },
 ]
 
 const ContactDetails = () => {
