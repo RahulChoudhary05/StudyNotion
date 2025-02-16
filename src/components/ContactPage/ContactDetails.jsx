020e0c59ff41dc80ee05379747d7a18e4de1f5f0
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2025-02-16 20:38:14 +0530

    add call us button in contactus page

--- src/components/ContactPage/ContactDetails.jsx
+++ src/components/ContactPage/ContactDetails.jsx
@@ -17,6 +17,12 @@ const contactDetails = [
     details:
       "Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016",
   },
+  {
+    icon: "IoCall",
+    heading: "Call us",
+    description: "Mon - Fri From 8am to 5pm",
+    details: "+123 456 7869",
+  },
 ]
 
 const ContactDetails = () => {
