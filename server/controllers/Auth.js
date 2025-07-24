b8013988c50dcf9f0ba44d209e76675969e533f5
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2025-07-24 15:25:43 +0530

    change bcrypt to bcrypt js

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const OTP = require("../models/OTP");
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
